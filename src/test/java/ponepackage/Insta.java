bfb81f66a8c876280badbb40565453040d2e7c64
shubhammangire123 <shubhammangire@gmail.com>
2023-08-02 21:53:54 +0530

    [shubhammangire123] adding pone project

---
+++ src/test/java/ponepackage/Insta.java
@@ -0,0 +1,9 @@
+package ponepackage;
+
+import org.testng.annotations.Test;
+
+public class Insta {
+  @Test
+  public void insta() {
+  }
+}
---
+++ src/test/java/ponepackage/fb.java
@@ -0,0 +1,9 @@
+package ponepackage;
+
+import org.testng.annotations.Test;
+
+public class fb {
+  @Test
+  public void f() {
+  }
+}
